55e91f48c1ad8b66952886ee51bd82d0b04f7aab
Rom555 <romanzubrilin@gmail.com>
2022-01-27 16:12:13 +0300

    init commit in lesson01

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = "Работа с гит";
+const screens = [123, 12, 1];
+const screenPrice = 12000;
+const rollback = "возврат";
+const fullPrice = 24000;
+const adaptive = "адаптив";
+
+alert("Первая работа с гит");
+
+console.log("Работа с гит");
