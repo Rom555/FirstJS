99539dd154d8b132decfe94f0782998f5893ffff
Rom555 <romanzubrilin@gmail.com>
2022-02-01 15:27:58 +0300

    code restyle

--- js/script.js
+++ js/script.js
@@ -1,24 +1,32 @@
 "use strict";
 
 const title = prompt("Как называется ваш проект?");
-const screens = prompt(
-  "Какие типы экранов нужно разработать?",
-  "Простые, Сложные, Интерактивные"
-);
-const screenPrice = parseInt(
-  prompt("Сколько будет стоить данная работа?", "12000")
-);
-const rollback = 55;
+const screens = prompt("Какие типы экранов нужно разработать?");
+const screenPrice = +prompt("Сколько будет стоить данная работа?");
 const adaptive = confirm("Нужен ли адаптив на сайте?");
-
 const service1 = prompt("Какой дополнительный тип услуги нужен?");
-const servicePrice1 = parseInt(prompt("Сколько это будет стоить?"));
+const servicePrice1 = +prompt("Сколько это будет стоить?");
 const service2 = prompt("Какой дополнительный тип услуги нужен?");
-const servicePrice2 = parseInt(prompt("Сколько это будет стоить?"));
+const servicePrice2 = +prompt("Сколько это будет стоить?");
+
+const rollback = 55;
 
 const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
 const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * rollback) / 100);
+
+switch (true) {
+  case fullPrice > 30000:
+    console.log("Даем скидку в 10%");
+    break;
+  case 15000 < fullPrice && fullPrice <= 30000:
+    console.log("Даем скидку в 5%");
+    break;
+  case 0 <= fullPrice && fullPrice <= 15000:
+    console.log("Скидка не предусмотрена");
+    break;
+  default:
+    console.log("Что то пошло не так");
+}
 
 console.log("title имеет тип данных " + typeof title);
 console.log("fullPrice имеет тип данных " + typeof fullPrice);
@@ -38,17 +46,3 @@ console.log(
 console.log(
   "Итоговая стоимость за вычетом отката посреднику " + servicePercentPrice
 );
-
-switch (true) {
-  case fullPrice > 30000:
-    console.log("Даем скидку в 10%");
-    break;
-  case 15000 < fullPrice && fullPrice <= 30000:
-    console.log("Даем скидку в 5%");
-    break;
-  case 0 <= fullPrice && fullPrice <= 15000:
-    console.log("Скидка не предусмотрена");
-    break;
-  default:
-    console.log("Что то пошло не так");
-}
